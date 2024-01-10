9ee44cb8ac594416f4e00ad067af1e64cfa38855
Juliano Zilli <juliano.zilli14@hotmail.com>
2024-01-10 00:21:10 -0300

    Update page.tsx

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -44,7 +44,7 @@ const LINKS = [
     description: "Choose from over 15 examples and deploy with a single click.",
   },
   {
-    title: "Deploy",
+    title: "wdsa",
     href: "https://vercel.com/new",
     description:
       " Instantly deploy your Turborepo to a shareable URL with Vercel.",
